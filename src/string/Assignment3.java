2c358a377dcc69a040ae9f6e0195a57862ed6679
Amit <apple@Apples-MacBook-Pro.local>
2022-08-27 12:22:40 +0530

    Issue resolved

--- src/string/Assignment3.java
+++ src/string/Assignment3.java
@@ -3,9 +3,17 @@ package string;
 public class Assignment3 {
     public static void main(String[] args) {
         String company = "TECHNOGISE";
-        for (int i = 1; i <= company.length() ;i=i+2) {
-           int count=0;
-            System.out.println(company.substring(count,i));
+        for (int i = 1; i <= company.length(); i = i + 2) {
+            for (int j = i; j <= i; j++) {
+                if(i==9){
+                    System.out.println(company.substring(0,j+1));
+                }
+                else {
+                    System.out.println(company.substring(0, j));
+                }
+
+            }
         }
     }
 }
+
